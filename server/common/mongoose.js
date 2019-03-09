c115fdca822033ba5762e06518d14756bf3e4998
Kevin <eilgwon88@gmail.com>
2019-03-10 00:12:00 +0900

    change image and fix error code

--- server/common/mongoose.js
+++ server/common/mongoose.js
@@ -4,7 +4,7 @@ const mongo = config.mongo[env];
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(mongo.url || "mongo://localhost:27017", mongo.option)
+  .connect(mongo.url || "mongodb://localhost:27017", mongo.option)
   .then(
     () => console.log("[", new Date(), "]", "Mongo Connection: OK"),
     err => console.error("[", new Date(), "]", "Mongo Connection: Error")
